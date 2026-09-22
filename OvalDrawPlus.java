20cc8612508163271af58175f7fca70ab4148161
Omar Sayyed <Omarsayyed87@gmail.com>
2026-09-22 12:12:52 -0500

    Updated File

--- OvalDrawPlus.java
+++ OvalDrawPlus.java
@@ -1,5 +1,6 @@
 import javax.swing.JFrame;
-
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class OvalDrawPlus {
     public static void main(String[] args) {
@@ -9,8 +10,13 @@ public class OvalDrawPlus {
         OvalDrawPlusWindow.setSize(400, 500); // width, height
         OvalDrawPlusWindow.setLocation(250, 150);
         OvalDrawPlusWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         OvalDrawPlusWindow.add(new OvalDrawPanel());
+        OvalDrawPlusWindow.add(new OvalDrawPanel());
         OvalDrawPlusWindow.setVisible(true);
+
+        OvalDrawPlusWindow.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.out.println("Closing OvalDrawPlus...");
+            }
+        });
     }
 }
-   
