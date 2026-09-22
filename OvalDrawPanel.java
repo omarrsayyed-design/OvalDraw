b9c33544442b59eb3d0a929bb58e011dca48b859
Omar Sayyed <Omarsayyed87@gmail.com>
2026-09-22 12:20:07 -0500

    Update

--- OvalDrawPanel.java
+++ OvalDrawPanel.java
@@ -4,8 +4,13 @@ import java.awt.Graphics;
 
 public class OvalDrawPanel extends JPanel {
 
+    private static final Color RECTANGLE_COLOR = new Color(0, 0, 139);
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        g.setColor(RECTANGLE_COLOR);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
         g.setColor(Color.GREEN);
         g.fillOval(0, 0, getWidth(), getHeight());
